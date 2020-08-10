6385bd4811ef4a7b42dc99efdf762871874608e9
Dogg1-terraform <69461766+Dogg1-terraform@users.noreply.github.com>
2020-08-10 13:32:41 +0200

    Delete provider1.tf

--- provider1.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-  access_key = "AKIASV6LSYDBXSQM6PPC" #AWS Access key
-  secret_key = "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" #AWS Secret Access Key
-  region = "${var.region}"  #The region in which you want to Operate
-}
